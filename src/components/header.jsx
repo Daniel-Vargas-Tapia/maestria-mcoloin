cf7522ae8e03444db870a563759cac74b3090545
dannisoul <dannitangerine@gmail.com>
2023-09-05 01:40:36 -0600

    2nd commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,7 @@ export function Header () {
 
     <header className={styles.mainHeader}>
       <div className={styles.backdrop} />
+      <div className={styles.backgroundHeader} />
       <Logos />
       <h1>Maestría en Comercio y Logística Internacional</h1>
       <p>Desarrollamos profesionales que entiendan la complejidad de la globalización, para mejorar el intercambio comercial de México</p>
