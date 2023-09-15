20dca7382dbd4b5db4723f69aa13b221a87503b3
dannisoul <dannitangerine@gmail.com>
2023-09-14 20:44:28 -0600

    correccion mapa curricular

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import styles from './navbar.module.css'
-import { IconHome2, IconFileCertificate, IconCalendar, IconUsersGroup, IconSchool, IconFileDescription, IconBook, IconTable } from '@tabler/icons-react'
+import { IconHome2, IconFileCertificate, IconCalendar, IconUsersGroup, IconSchool, IconFileDescription, IconBook, IconMap } from '@tabler/icons-react'
 export function Navbar ({ sectionHandler, section }) {
   return (
     <nav>
@@ -10,7 +10,7 @@ export function Navbar ({ sectionHandler, section }) {
         <li><button onClick={() => sectionHandler('requisitos_ingreso')} className={section === 'requisitos_ingreso' ? styles.active : ''}> <IconFileDescription /> <span>Requisitos de Ingreso</span></button></li>
         <li><button onClick={() => sectionHandler('fecha_admision')} className={section === 'fecha_admision' ? styles.active : ''}><IconCalendar /><span>Fecha de Admisión</span></button></li>
         <li><button onClick={() => sectionHandler('plan_estudios')} className={section === 'plan_estudios' ? styles.active : ''}><IconSchool /><span>Plan de Estudios</span></button></li>
-        <li><button onClick={() => sectionHandler('mapa_curricular')} className={section === 'mapa_curricular' ? styles.active : ''}><IconTable /><span>Mapa Curricular</span></button></li>
+        <li><button onClick={() => sectionHandler('mapa_curricular')} className={section === 'mapa_curricular' ? styles.active : ''}><IconMap /><span>Mapa Curricular</span></button></li>
         <li><button onClick={() => sectionHandler('nucleo_academico')} className={section === 'nucleo_academico' ? styles.active : ''}><IconUsersGroup /><span>Núcleo Académico</span></button></li>
       </ul>
     </nav>
