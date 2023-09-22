ce2928f12bc7dec556397b410a0d424bc9c4ca5d
dannisoul <dannitangerine@gmail.com>
2023-09-21 23:23:03 -0600

    animacion header corregida

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,14 +5,19 @@ export function Header () {
   return (
 
     <header className={styles.mainHeader} id='mainHeader'>
-      <div className={styles.backdrop} />
-      <div className={styles.backgroundHeader} />
+      <img className={styles.headerIMG} src='/headers/header.webp' />
+      <img className={styles.headerIMG} src='/headers/header2.webp' />
+      <img className={styles.headerIMG} src='/headers/header3.webp' />
+      <img className={styles.headerIMG} src='/headers/header4.webp' />
+      <img className={styles.headerIMG} src='/headers/header5.webp' />
+
       <Logos />
       <h1>Maestría en Comercio y Logística Internacional</h1>
       <p>Desarrollamos profesionales que entiendan la complejidad de la globalización, para mejorar el intercambio comercial de México</p>
       <div className={styles.socialLinksContainer}>
         <SocialLinks />
       </div>
+      <div className={styles.backdrop} />
     </header>
 
   )
