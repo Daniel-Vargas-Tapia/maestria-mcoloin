504c58b34e7a97a4a3c0966ce733e0c5f2f0ace0
EOmar08 <arredondoomar101@hotmail.com>
2023-09-22 10:45:02 -0600

    CV y foto de arely

--- src/pages/requisitos_ingreso.jsx
+++ src/pages/requisitos_ingreso.jsx
@@ -7,11 +7,11 @@ export function RequisitosIngreso () {
         <h2>Requisitos de Ingreso</h2>
         <ul>
           <li>Requisitos académicos (habilidades cognoscitivas).</li>
-          <li>Contar con una licenciatura a fin al área.</li>
-          <li>Presentar curriculum vitae.</li>
+          <li>Contar con una licenciatura afín al área.</li>
+          <li>Curriculum vitae.</li>
           <li>Demostrar experiencia profesional en actividades de tráfico, logística, despacho aduanero e importaciones y exportaciones en general.</li>
           <li>Presentar exámen de admisión.</li>
-          <li>Presentar carta de exposición de motivos.</li>
+          <li>Carta de exposición de motivos.</li>
           <li>Presentación del protocolo de investigación al núcleo básico del Posgrado.</li>
         </ul>
       </div>
