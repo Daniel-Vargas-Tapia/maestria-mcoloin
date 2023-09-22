c697c5f3cb11f5607a41607c5401d8582a071717
EOmar08 <arredondoomar101@hotmail.com>
2023-09-22 10:08:45 -0600

    Cambio de tamaño de flyer

--- src/pages/fecha_admision.jsx
+++ src/pages/fecha_admision.jsx
@@ -2,7 +2,7 @@ import styles from './fecha_admision.module.css'
 export function FechaAdmision () {
   return (
     <section className={styles.fechasSeccion}>
-      <img className={styles.imgFlyer} src='./flyers/Flyer.png' />
+      <img className={styles.imgFlyer} src='./flyers/Flyer.webp' />
     </section>
   )
 }
