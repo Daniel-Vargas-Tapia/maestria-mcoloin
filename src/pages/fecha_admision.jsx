b96a5521c19fadf2fd30f4fecce1901542b9209f
miguelarj12 <miguelaguilas12@hotmail.com>
2023-10-13 10:35:09 -0600

    correcciones de slider

--- src/pages/fecha_admision.jsx
+++ src/pages/fecha_admision.jsx
@@ -2,11 +2,9 @@ import styles from './fecha_admision.module.css'
 export function FechaAdmision () {
   return (
     <section className={styles.fechasSeccion}>
-      <div className={styles.divslider}>
-        <div><img className={styles.imgslider} src='./flyers/Slider.webp' /></div>
-        <a href='https://uptexcoco.edomex.gob.mx/sites/uptexcoco.edomex.gob.mx/files/files/2023/posgrado.pdf' target='_blank' rel='noopener noreferrer' className={styles.btnver}>Ver mas</a>
-      </div>
-      <div><img className={styles.imgFlyer} src='./flyers/Flyer.webp' /></div>
+      <img className={styles.imgFlyer} src='./flyers/Slider.webp' />
+      <a className={styles.convocatoria} href='https://uptexcoco.edomex.gob.mx/sites/uptexcoco.edomex.gob.mx/files/files/2023/posgrado.pdf' target='_blank' rel='noopener noreferrer'>Ver más </a>
+      <img className={styles.imgFlyer} src='./flyers/Flyer.webp' />
     </section>
   )
 }
